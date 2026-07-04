c0dc3338c8052155c7b8769c4a4b22d434926130
Thomas Davidsen <260642635+Thomas-H-D@users.noreply.github.com>
2026-07-04 17:06:13 +0900

    finished Part 6 of the Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,8 @@ Rails.application.routes.draw do
   get "tasks/:id", to: "tasks#show", as: :task
   # create -> the POST version
   post "tasks", to: "tasks#create"
+  # edit
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # update
+  patch "tasks/:id", to: "tasks#update"
 end
